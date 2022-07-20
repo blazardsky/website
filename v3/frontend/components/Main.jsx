b2555e9d596138a07356bc960886b3f9d103b4d0
Niccolò Agnoletti <hsnicco@gmail.com>
2022-07-21 00:25:39 +0200

    css fixes

--- v3/frontend/components/Main.jsx
+++ v3/frontend/components/Main.jsx
@@ -1,10 +1,10 @@
 export default function Main({h1,h2}){
     return(
-        <main className="relative p-5 w-100 flex content-center justify-center">
-        <div clasname="mx-auto">
-          <h1 className="punk-font text-center text-white text-8xl tracking-wider mt-9">{h1}</h1>
-          <h2 className="font-bold font-mono text-center uppercase text-white">{h2}</h2>
-        </div>
+        <main className="relative mx-auto w-fit max-w-screen flex content-center justify-center">
+          <div className="p-6">
+            <h1 className="punk-font text-center text-white text-5xl md:text-7xl xl:text-8xl tracking-wider mt-9">{h1}</h1>
+            <h2 className="font-bold font-mono text-center uppercase text-white px-1">{h2}</h2>
+          </div>
       </main>
     )
 }
--- v3/frontend/pages/index.js
+++ v3/frontend/pages/index.js
@@ -50,7 +50,7 @@ export default function Home(props) {
       <Main h1={title} h2={tagline} />
       <section>
         <div className="p-14 m-5 border-2 rounded-0 border-black border-solid -skew-y-3 w-auto max-w-fit relative bg-yellow-400">
-          <h3 className="absolute top-6 -right-5 skew-y-4 -skew-x-12 border-2 rounded-0 bg-pink-600 text-white border-black border-solid py-1 px-3 tracking-wide font-mono font-semibold">
+          <h3 className="absolute top-6 -right-3 md:-right-5 skew-y-4 -skew-x-12 border-2 rounded-0 bg-pink-600 text-white border-black border-solid py-1 px-3 tracking-wide font-mono font-semibold">
             Mi presento
           </h3>
           <div className="skew-y-3 prose prose-p:text-black prose-h3:font-bold prose-h3:text-2xl prose-h4:italic prose-h4:text-xl">
