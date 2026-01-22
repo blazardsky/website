ebc7507efe9f17b290e387d2653ccb9f4780bdab
Nic <blazardsky@gmail.com>
2026-01-22 12:09:26 +0100

    v2 branch with only v2 folder content

--- v4/web/src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="@sanity/astro/module" />
-
-// Add this line to add typings for GSAP
-declare const gsap: typeof import("gsap");
