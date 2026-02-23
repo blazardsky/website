f36cc60a2f077647d56d3cdfa8c9a2461564ea81
Nic <nic.agnoletti@gmail.com>
2026-02-23 14:45:04 +0100

    tailwind fixes + updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 import partytown from "@astrojs/partytown";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nicc-olo.com",
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
     react(),
     partytown(),
